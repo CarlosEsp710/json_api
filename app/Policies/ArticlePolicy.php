1bc8847264d5ff5f0b81c56be211726ed5a1f438
Carlos Espejel <carlosespejel71@gamil.com>
2021-08-25 18:54:05 -0500

    Refactorizamos las políticas de acceso

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -11,11 +11,11 @@ class ArticlePolicy
 
     public function update(User $user, $article)
     {
-        return $user->id === $article->user_id;
+        return $article->user->is($user);
     }
 
     public function delete(User $user, $article)
     {
-        return $user->id === $article->user_id;
+        return $article->user->is($user);
     }
 }
